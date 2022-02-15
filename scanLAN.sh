d39faebd4612b445e615458cbcfc36ff104cedd2
ruberak <ruberak@mail.uc.edu>
2022-02-15 17:46:50 +0000

    automate scan

---
+++ scanLAN.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+#Capstone Project IOT Filter
+#Client Pi Script
+#This Script checks to see if user requested LAN scan from APP interface
+
+#Connect to AWS and get scancheck for scan execute?
+sftp testuser01@ec2-3-86-210-241.compute-1.amazonaws.com:SCAN/scanCheck.txt ../TEMP/
+#Store contents of scancheck.txt in var for scan execute?
+scanFlag=$(cat ../TEMP/scanCheck.txt)
+#Run network scan nmap if scanFlag is 1 or exit script if scanflag is 0
+if [ $scanFlag -eq '1' ]; then
+	#scan network 
+	sudo nmap -sP 192.168.50.1-254 > ../RESULTS/scanResults.txt 
+	#copy results to AWS
+	scp ../RESULTS/scanResults.txt testuser01@ec2-3-86-210-241.compute-1.amazonaws.com:SCAN/
+	#reset scanFlag in AWS scancheck.txt to zero
+	echo 0 | ssh testuser01@ec2-3-86-210-241.compute-1.amazonaws.com 'cat >| SCAN/scanCheck.txt' 
+fi
