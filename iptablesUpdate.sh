0ebdc3920d776d22955e6742b24ea6d433f3e16f
ruberak <ruberak@mail.uc.edu>
2022-03-16 05:29:00 +0000

    fixed while loop rule application and added log write block

--- iptablesUpdate.sh
+++ iptablesUpdate.sh
@@ -15,13 +15,13 @@ if [ $ruleUpdateFlag -eq '1' ]; then
 	#Connect to AWS and get the latest copy of ruleUpdates.txt for local client pi iptables changes 
 	sftp testuser01@ec2-3-86-210-241.compute-1.amazonaws.com:RULES/ruleUpdates.txt ../TEMP/ 
 	#read ruleUpdates.txt and loop through each line
-	lines=$(cat ../TEMP/ruleUpdates.txt)
-	for line in $lines
-	do
+	rules=/home/pi/TEMP/ruleUpdates.txt
+	while IFS= read -r line; do
+		#remove newline chars from line
+		line=$(echo $line | tr -d '\r\n')
 		#split line into two parts rule toggle and mac address
 		activeRule=${line:0:1}
 		macAddr=${line:2}
-		
 		#check activeRule value, 0 rule is remove, 1 rule is added
 		if [ $activeRule -eq '1' ]; then
 			sudo iptables -A FORWARD -m mac --mac-source $macAddr -i wlan0 -j DROP
@@ -30,12 +30,15 @@ if [ $ruleUpdateFlag -eq '1' ]; then
 		else
 			echo "Fatal Error"
 		fi
-	done
+	done < $rules
 
 	#Update iptables config file with new rules, this is incase power is lost to clientpi
 	sudo netfilter-persistent save
 	#overwrite ruleUpdates.txt to blank file
 	ssh testuser01@ec2-3-86-210-241.compute-1.amazonaws.com 'cat /dev/null >| RULES/ruleUpdates.txt'
 	#reset ruleUpdateFlag in AWS ruleCheck.txt to zero
-	echo 0 | ssh testuser01@ec2-3-86-210-241.compute-1.amazonaws.com 'cat >| RULES/ruleCheck.txt' 
+	echo 0 | ssh testuser01@ec2-3-86-210-241.compute-1.amazonaws.com 'cat >| RULES/ruleCheck.txt'
+       #get date and format, then write to log file that rules were applied
+	timeStamp=$(TZ='America/New_York' date +"%b %d %H:%M")
+ 	echo "$timeStamp Filtering Change Applied" | ssh testuser01@ec2-3-86-210-241.compute-1.amazonaws.com 'cat >> /home/testuser01/LOG/log.txt'	
 fi
