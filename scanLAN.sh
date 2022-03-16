8c424b2e956fd37a0cf6b9e83c3056f87e70782f
ruberak <ruberak@mail.uc.edu>
2022-03-16 05:29:35 +0000

    added log write block

--- scanLAN.sh
+++ scanLAN.sh
@@ -8,6 +8,8 @@
 sftp testuser01@ec2-3-86-210-241.compute-1.amazonaws.com:SCAN/scanCheck.txt ../TEMP/
 #Store contents of scancheck.txt in var for scan execute?
 scanFlag=$(cat ../TEMP/scanCheck.txt)
+#get date format for log write
+timeStamp=$(TZ='America/New_York' date +"%b %d %H:%M")
 #Run network scan nmap if scanFlag is 1 or exit script if scanflag is 0
 if [ $scanFlag -eq '1' ]; then
 	#scan network 
@@ -15,5 +17,8 @@ if [ $scanFlag -eq '1' ]; then
 	#copy results to AWS
 	scp ../RESULTS/scanResults.txt testuser01@ec2-3-86-210-241.compute-1.amazonaws.com:SCAN/
 	#reset scanFlag in AWS scancheck.txt to zero
-	echo 0 | ssh testuser01@ec2-3-86-210-241.compute-1.amazonaws.com 'cat >| SCAN/scanCheck.txt' 
+	echo 0 | ssh testuser01@ec2-3-86-210-241.compute-1.amazonaws.com 'cat >| SCAN/scanCheck.txt'
+       #write to log that scan has completed
+	echo "$timeStamp Scan has Completed" | ssh testuser01@ec2-3-86-210-241.compute-1.amazonaws.com 'cat >> /home/testuser01/LOG/log.txt'
+	       
 fi
